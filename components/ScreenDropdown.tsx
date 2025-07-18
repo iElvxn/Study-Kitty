fa4da63bbe6238469b53451905405da90c03bc7e
Elvin Ly <elvinly323@gmail.com>
2025-07-17 21:20:26 -0400

    Change drop down menu home to timer:

--- components/ScreenDropdown.tsx
+++ components/ScreenDropdown.tsx
@@ -8,7 +8,7 @@ import { ThemedText } from './ThemedText';
 import { IconSymbol } from './ui/IconSymbol';
 
 const SCREENS = [
-  { name: 'Home', route: '/' as const, icon: 'house.fill' as const },
+  { name: 'Timer', route: '/' as const, icon: 'house.fill' as const },
   { name: 'Adopt Cats', route: '/adopt' as const, icon: 'paperplane.fill' as const },
   { name: 'Purrdex', route: '/purrdex' as const, icon: 'person.fill' as const },
 ] as const;
